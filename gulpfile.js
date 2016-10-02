a34695c170fe7606f4619d10f46c96281ebcc76d
Ryan Fitzgerald <rfitz042@uottawa.ca>
2016-10-02 13:04:09 -0400

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var autoprefix = require('gulp-autoprefixer');
+
+gulp.task('compress', function() {
+  return gulp.src('liquo/liquo.js')
+    .pipe(uglify())
+    .pipe(rename({extname: '.min.js'}))
+    .pipe(gulp.dest('liquo'));
+});
+
+gulp.task('sass', function() {
+  return gulp.src('liquo/liquo.scss')
+    .pipe(sass())
+    .pipe(autoprefix())
+    .pipe(gulp.dest('liquo'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('liquo/*.js', ['compress']);
+  gulp.watch('liquo/*.scss', ['sass']);
+});
