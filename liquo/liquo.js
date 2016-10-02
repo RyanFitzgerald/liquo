1662944e7b1fc5537450c43cfdef9eaf717583f7
Ryan Fitzgerald <rfitz042@uottawa.ca>
2016-10-02 13:20:05 -0400

    Made some additional initial setups

---
+++ liquo/liquo.js
@@ -0,0 +1,30 @@
+/*
+    Liquo - Filterable and Responsive Image Gallery
+    Version 1.0.0
+    Ryan Fitzgerald
+    https://RyanFitzgerald.ca/
+    ---
+    Repo: http://github.com/ryanfitzgerald/liquo
+    Issues: http://github.com/ryanfitzgerald/liquo/issues
+    Licensed under MIT Open Source
+ */
+
+(function($) {
+
+    $.fn.liquo = function(options) {
+
+        // Override defaults, if provided
+        var settings = $.extend({
+
+        }, options);
+
+        // Allow chaining and process each DOM node
+        return this.each(function() {
+
+            
+
+        });
+
+    }
+
+})(jQuery);
