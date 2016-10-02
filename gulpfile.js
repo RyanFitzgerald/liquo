849afe336307890ef3fd696163708eb83de2abac
Ryan Fitzgerald <rfitz042@uottawa.ca>
2016-10-02 14:09:51 -0400

    Started working on layout and core functionality

--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,20 @@ var rename = require('gulp-rename');
 var autoprefix = require('gulp-autoprefixer');
 
 gulp.task('compress', function() {
-  return gulp.src('liquo/liquo.js')
-    .pipe(uglify())
-    .pipe(rename({extname: '.min.js'}))
-    .pipe(gulp.dest('liquo'));
+    return gulp.src('liquo/liquo.js')
+        .pipe(uglify())
+        .pipe(rename({extname: '.min.js'}))
+        .pipe(gulp.dest('liquo'));
 });
 
 gulp.task('sass', function() {
-  return gulp.src('liquo/liquo.scss')
-    .pipe(sass())
-    .pipe(autoprefix())
-    .pipe(gulp.dest('liquo'));
+    return gulp.src('liquo/liquo.scss')
+        .pipe(sass())
+        .pipe(autoprefix())
+        .pipe(gulp.dest('liquo'));
 });
 
-gulp.task('watch', function() {
-  gulp.watch('liquo/*.js', ['compress']);
-  gulp.watch('liquo/*.scss', ['sass']);
+gulp.task('watch', ['compress', 'sass'], function() {
+    gulp.watch('liquo/*.js', ['compress']);
+    gulp.watch('liquo/*.scss', ['sass']);
 });
--- liquo/liquo.js
+++ liquo/liquo.js
@@ -15,13 +15,40 @@
 
         // Override defaults, if provided
         var settings = $.extend({
-
+            menu: null
         }, options);
+
+        // Throw error if menu not defined
+        if (settings.menu == null) throw "Liquo: You must provide a valid menu selector.";
 
         // Allow chaining and process each DOM node
         return this.each(function() {
 
-            
+            // Define variables
+            var $this = $(this); // Store reference to self
+            var $menu = $(settings.menu);
+            var $options = $menu.children("a[data-liquo]");
+
+            // Add liquo classes
+            $this.addClass("liquo-gallery");
+
+            // Handle menu functionality
+            $($options).click(function(e) {
+
+                // Stop link
+                e.preventDefault();
+
+                // Get category
+                var category = $(this).data("liquo");
+
+                // Get category value
+                if (category == "all") {
+                    alert("All");
+                } else {
+                    alert(category);
+                }
+
+            });
 
         });
 
