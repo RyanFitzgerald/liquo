ef12d8733abfa03545fa661ea723e0b98ae67426
Ryan Fitzgerald <rfitz042@uottawa.ca>
2016-10-03 18:33:14 -0400

    Added additional options

--- liquo/liquo.js
+++ liquo/liquo.js
@@ -15,7 +15,10 @@
 
         // Override defaults, if provided
         var settings = $.extend({
-            menu: null
+            menu: null,
+            theme: null,
+            start: "all",
+            random: false
         }, options);
 
         // Throw error if menu not defined
@@ -30,40 +33,84 @@
             var $options = $menu.children("a[data-liquo]");
             var last = null;
 
-            // Add liquo classes
+            // Add liquo classe
             $this.addClass("liquo-gallery");
 
-            // Add active class to "all"
-            $menu.children("a[data-liquo=all]").addClass("active");
+            // Add theme if provided
+            if (settings.theme != null) $this.addClass(theme);
+
+            // Add active class to "all" if no default provided
+            if (settings.start != "all" && $menu.children("a[data-liquo="+settings.start+"]").length > 0) {
+
+                $menu.children("a[data-liquo="+settings.start+"]").addClass("liquo-active");
+
+                $this.find("li").not("[data-liquo="+settings.start+"]").addClass("inactive").hide(300);
+
+                last = settings.start;
+
+            } else {
+
+                $menu.children("a[data-liquo=all]").addClass("liquo-active");
+
+            }
+
+            // Randomize if true
+            if (settings.random) {
+
+                $this.children("li").sort(function(a,b) {
+
+                    return (Math.round(Math.random())-0.5);
+
+                }).detach().appendTo($this);
+
+            }
 
             // Handle menu functionality
             $($options).click(function(e) {
 
-                // Stop link
+                // Stop link functionality
                 e.preventDefault();
 
                 // Get category
                 var category = $(this).data("liquo");
 
                 // Change active classes
-                if (!$(this).hasClass("active")) {
-                    $menu.find(".active").removeClass("active");
-                     $(this).addClass("active");
+                if (!$(this).hasClass("liquo-active")) {
+
+                    // Remove currently active link
+                    $menu.find(".liquo-active").removeClass("liquo-active");
+
+                    // Add new active link
+                    $(this).addClass("liquo-active");
                 }
 
                 // Get category value
                 if (category == "all") {
-                    $this.find("li").fadeIn(300).removeClass("inactive");
+
+                    $this.find("li").show(300).removeClass("inactive");
+
                     last = null;
-                } else if (last == null) {
-                    $this.find("li").not("[data-liquo="+category+"]").addClass("inactive").hide(300);
+
+                } else if ($this.find("li[data-liquo="+category+"]").length > 0) {
+
+                    if (last == null) {
+
+                        $this.find("li").not("[data-liquo="+category+"]").addClass("inactive").hide(300);
+
+                    } else if (category != last) {
+
+                        $this.find("li[data-liquo="+last+"]").addClass("inactive").hide(300, function() {
+
+                            $this.find("li[data-liquo="+category+"]").removeClass("inactive").show(300);
+
+                        });
+                    }
+
                     last = category;
-                } else if (category != last) {
-                    $this.find("li[data-liquo="+last+"]").addClass("inactive").hide(300, function() {
-                        $this.find("li[data-liquo="+category+"]").removeClass("inactive").show(300);
-                    });
-                    last = category;
+
                 }
+
+
 
             });
 
